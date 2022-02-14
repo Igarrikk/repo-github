92619fbd18fe1d71c6f04a20550d63109ec5bc19
Igarrik <avorahas@gmail.com>
2022-02-14 18:14:35 +0300

    Create README.md

--- 1.py
+++ 1.py
@@ -10,8 +10,8 @@ name = input('Введите имя: ')
 age = int(input('Сколько вам лет?: '))
 period = 50
 age_period = age + period
-stroka = 'Привет, ' + name + ', через ' + str(period) + ' лет Вам будет ' + str(age_period)
+stroka = 'Привет, ' + name + ', через ' + str(period) + ' лет Вам будет ' + str(age_period)  # 1 метод
 print(stroka)
-stroka = 'Привет, {}, через {} лет Вам будет {} ' .format(name, period, age_period)
+stroka = 'Привет, {}, через {} лет Вам будет {} ' .format(name, period, age_period)  # 2 метод
 print(stroka)
 
